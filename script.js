adf7bc679ff37b18fa479229e366df9391bbca39
Akankxh <94113177+Akankxh@users.noreply.github.com>
2022-06-04 09:41:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,102 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let keySequence;
+    let clickEnable = false;
+    let gameOver = false;
+    let roundClicks = 0;
+  
+  console.log("test");
+    let playAgainButton = document.querySelector("button");
+    let introText = document.querySelector("#randtext");
+    let outroText = document.querySelector("#gameover");
+    playAgainButton.style.display = "none";
+    outroText.style.display = "none";
+    document.querySelector("#gameover").textContent = "GAME OVER!!";
+    document.querySelector("#randtext").textContent =
+      "Click on Highlighted tiles in correct order";
+    keySequence = new Map();
+    playRound();
+  
+    let allKeys = document.querySelectorAll("td");
+    allKeys.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        if (clickEnable) clickToLight(e);
+      });
+    });
+  
+    //function to light the tile on clicking
+    function clickToLight(e) {
+      roundClicks++;
+  
+      const key = e.target;
+      key.style.backgroundColor = "lightblue";
+  
+      if (e.target.getAttribute("id") === keySequence.get(roundClicks)) {
+        key.style.backgroundColor = "lightblue";
+        setTimeout(() => {
+          key.style.backgroundColor = "black";
+        }, 500);
+      } else {
+        key.style.backgroundColor = "rgb(255,0,0)";
+        gameOver = true;
+        clickEnable = false;
+        playAgainButton.style.display = "block";
+        introText.style.display = "none";
+        outroText.style.display = "block";
+      }
+  
+      if (keySequence.size === roundClicks && !gameOver) {
+        playRound();
+      }
+  
+      console.log(keySequence);
+  
+      setTimeout(() => {
+        key.style.backgroundColor = "black";
+      }, 500);
+    }
+  
+    //function to generate random numbers
+    function randomNum() {
+      let rand = Math.random() * 16;
+      return Math.floor(rand) + 1;
+    }
+  
+    function addToSequence() {
+      const randomKeyNumber = randomNum();
+      keySequence.set(keySequence.size + 1, `key-${randomKeyNumber}`);
+    }
+  
+    function lightSequence() {
+      clickEnable = false; //player cannot click while sequence is being lit
+  
+      setTimeout(() => {
+        clickEnable = true;
+        roundClicks = 0; //here we are resetting number of clicks to 0
+      }, keySequence.size * 1000);
+      keySequence.forEach((value, key) => {
+        const k = document.querySelector(`#${value}`);
+        console.log(k);
+        setTimeout(() => {
+          lightKey(k);
+        }, 1000 * key);
+      });
+    }
+  
+    function lightKey(key) {
+      key.style.backgroundColor = "lightblue";
+  
+      console.log(keySequence);
+  
+      setTimeout(() => {
+        key.style.backgroundColor = "black";
+      }, 500);
+    }
+  
+    function playRound() {
+      addToSequence();
+      document.querySelector(
+        "#displayRound"
+      ).textContent = `ROUND: ${keySequence.size}`; //diplays round number (scoring system of the game)
+      lightSequence();
+    }
+  });
